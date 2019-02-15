3c1d8c89849186c8c1640e1dc2dca822c40e1747
Valdo MR <valdomr.inco@gmail.com>
2019-02-15 09:13:55 -0600

    lexico solucionado (ya no es necesario poner espacio entre cada palabra)

--- compilador.js
+++ compilador.js
@@ -41,7 +41,7 @@ var fin_cadena = [
     ';'
 ];
 
-var identificador = /^[_a-zA-Z][_a-zA-Z0-9]*/g;
+var identificador = /^[_a-zA-Z][_a-zA-Z0-9]*$/g;
 
 var entero = /^-?[0-9]+$/g;
 
@@ -82,21 +82,37 @@ $(document).ready(function() {
         var cadena = '';
         var imprimir = '';
         var tipo = null;
+        var tipo2 = null;
+        nueva_cadena = nueva_cadena.replace(/\n/g, '');
         if (!nueva_cadena) {
             return;
         }
         for (var j = 0; j < nueva_cadena.length; j++) {
             cadena += nueva_cadena[j];
-            if (nueva_cadena[j] == '\n') {
-                cadena = cadena.slice(j, cadena.length - 1);
+            if (cadena[j] == '.') {
+                continue;
             }
             tipo = hacerMatch(cadena);
-            if (tipo) {
+            if (!tipo) {
+                tipo = hacerMatch(imprimir);
+                console.log(tipo);
+                recursivo(nueva_cadena.slice(j, nueva_cadena.length));
+                return;
+            }
+            if (tipo[0] == 'Palabra reservada' && nueva_cadena[j + 1]) {
+                tipo2 = hacerMatch(nueva_cadena[j + 1]);
+                if (tipo2 && (tipo2[0] != 'Identificador' && tipo2[0] != 'Numero entero')) {
+                    console.log(tipo);
+                    recursivo(nueva_cadena.slice(j + 1, nueva_cadena.length));
+                    return;
+                }
+            } else if (tipo) {
                 imprimir = cadena;
             } else {
                 tipo = hacerMatch(imprimir);
                 console.log(tipo);
                 recursivo(cadena.slice(j, cadena.length));
+                return;
             }
         }
         if (tipo) {
